53f551af03157e0fccead1dbdc29ee0914c36f87
Andy Wang <turtlewang3@gmail.com>
2024-07-09 14:52:31 -0400

    test and bank account class complete

--- bankaccount.py
+++ bankaccount.py
@@ -3,7 +3,7 @@ import numpy
 class BankAccount:
     def __init__(self,name, accountNumber):
         if(not isinstance(name,str)):
-            print("Please enter a valid name")
+            raise Exception("Please enter a valid name")
             return
         
         self.name = name
@@ -11,29 +11,30 @@ class BankAccount:
         self.balance = 0
 
         if(not isinstance(accountNumber,int)):
-            print("Please enter a valid account number")
+            raise Exception("Please enter a valid account number")
             return
         
         self.accountNumber = accountNumber
 
     def withdraw(self,amount):
-        if amount > 0:
-            try:
-                self.balance = self.balance - amount
-            except:
-                print("Please enter a valid number")
-        else:
-            print("Please enter a valid number")
+        if amount <= 0: 
+            raise Exception("Error: Please enter a valid number")
+        if amount > self.balance:
+            raise Exception("Error: Insufficient balance")
 
+        try:
+            self.balance = self.balance - amount
+        except:
+            raise Exception("Error: Please enter a valid number")
 
     def deposit(self,amount):
-        if amount > 0:
-            try:
-                self.balance = self.balance + amount
-            except:
-                print("Please enter a valid number")
-        else:
-            print("Please enter a valid number")
+        if amount <= 0: 
+            raise Exception("Error: Please enter a valid number")
+
+        try:
+            self.balance = self.balance + amount
+        except:
+            raise Exception("Error: Please enter a valid number")
 
     def printBal(self):
         print(self.balance)
--- test_bankaccount.py
+++ test_bankaccount.py
@@ -3,11 +3,41 @@ import bankaccount
 
 
 class TestBankAccount(unittest.TestCase):
-    def test_bankaccount(self):
-        myAccount = bankaccount.BankAccount("bruh",543,3848)
+    def test_bank_init(self):
+        myAccount = bankaccount.BankAccount("bruh", 3848)
         self.assertEqual(myAccount.name, "bruh")
-        self.assertEqual(myAccount.balance, 543)
+        self.assertNotEqual(myAccount.name, "lol")
+
+        self.assertEqual(myAccount.balance, 0)
+        self.assertNotEqual(myAccount.balance, 500)
+
         self.assertEqual(myAccount.accountNumber, 3848)
+        self.assertNotEqual(myAccount.accountNumber, 3844)
+
+    def test_bank_deposit(self):
+        myAccount = bankaccount.BankAccount("bruh", 3848)
+        myAccount.deposit(50)
+        self.assertEqual(myAccount.balance, 50)
+        self.assertNotEqual(myAccount.balance, 1)
+
+        myAccount.deposit(-50)
+        self.assertEqual(myAccount.balance, 50)
+        self.assertNotEqual(myAccount.balance, 0)
+
+    def test_bank_withdraw(self):
+        myAccount = bankaccount.BankAccount("bruh", 3848)
+        self.balance = 100
+        myAccount.withdraw(50)
+        self.assertEqual(myAccount.balance, 50)
+        self.assertNotEqual(myAccount.balance, 150)
+
+        myAccount.withdraw(-100)
+        self.assertEqual(myAccount.balance, 50)
+        self.assertNotEqual(myAccount.balance, 150)
+
+        myAccount.withdraw(5000)
+        self.assertEqual(myAccount.balance, 50)
+        self.assertNotEqual(myAccount.balance, 0)
 
 if __name__ == "__main__":
     unittest.main()
