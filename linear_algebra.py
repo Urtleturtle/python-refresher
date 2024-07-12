076ed25cd1a5acabf2c006f6c41c31f8bc765df1
Andy Wang <turtlewang3@gmail.com>
2024-07-12 07:41:16 -0400

    d

--- linear_algebra.py
+++ linear_algebra.py
@@ -13,24 +13,24 @@ print(f"Prob 1: \nSum of vectors a and b: {sum_vector}")
 
 # Subtraction of vectors
 diff_vector = a - b
-print(f"Difference of vectors a and b: {diff_vector}")
+print(f"Difference of vectors a and b: {diff_vector}\n")
 
 sum_vector = A + B
 print(f"Prob 2: \nSum of vectors a and b: {sum_vector}")
 
 # Subtraction of vectors
 diff_vector = A - B
-print(f"Difference of vectors a and b: {diff_vector}")
+print(f"Difference of vectors a and b: {diff_vector}\n")
 
-print(f"Prob 3: \n Dot of vectors a and b: {np.dot(a,b)}")\
+print(f"Prob 3: \n Dot of vectors a and b: {np.dot(a,b)}\n")\
 
 a = np.array([[1, 2, 3], [4,5,6]])
 b = np.array([[7,8,9,10],[11,12,13,14],[15,16,17,18]])
 
-print(f"Prob 4: \n Dot of vectors a and b: {np.dot(a,b)}")
+print(f"Prob 4: \n Dot of vectors a and b: {np.dot(a,b)}\n")
 
 a = np.array([1,1,2])
-print(f"Prob 5: \n Magntitude = {np.sqrt(a.dot(a))}")
+print(f"Prob 5: \n Magntitude = {np.sqrt(a.dot(a))}\n")
 
 a = np.array([[1,2],[3,4]])
-print(f"Prob 6\n tranpose is {np.transpose(a)}")
+print(f"Prob 6\n tranpose is {np.transpose(a)}\n")
